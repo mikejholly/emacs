d345819389cd0628c3f6ea33b182e05ba12eebd1
Mike Holly <mholly@digitalocean.com>
2017-02-28 13:48:12 -0800

    initial commit

---
+++ elpa/dracula-theme-20170210.830/dracula-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dracula-theme" "20170210.830" "Dracula Theme" '((emacs "24")) :url "https://github.com/dracula/emacs")
---
+++ init.el
@@ -0,0 +1,69 @@
+;; Add melpa
+(package-initialize)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.org/packages/"))
+
+;; Vertical completions
+(require 'ido-vertical-mode)
+(ido-mode 1)
+(ido-vertical-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+;; Disable UI
+(toggle-scroll-bar -1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+
+;; Default theme
+(load-theme 'dracula t)
+
+;; Open scratch buffer first
+(setq inhibit-startup-screen t)
+
+;; Start projectile
+(projectile-mode 1)
+
+;; Snippets
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Get PATH from shell
+(exec-path-from-shell-initialize)
+
+;; Winner undo keys
+(global-set-key (kbd "M-[") 'winner-undo)
+(global-set-key (kbd "M-]") 'winner-redo)
+
+;; Ace window
+(global-set-key (kbd "M-p") 'ace-window)
+(setq aw-background nil)
+
+;; Custom goimports
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+;; Tree style directories
+(require 'direx)
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory)
+
+;; Use y/n for prompt
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Whitespace
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq-default tab-width 4)
+
+;; Auto-complete keys
+(require 'company)
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
+;; Go tests
+(global-set-key (kbd "C-c .") 'go-test-current-test)
+
+;; smex
+(global-set-key (kbd "M-x") 'smex)
