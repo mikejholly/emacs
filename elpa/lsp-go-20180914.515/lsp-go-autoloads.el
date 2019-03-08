6a69e5167b56ce8baaa01edb3abf53e5ad5e577a
Mike <mholly@digitalocean.com>
2019-03-08 11:48:49 -0800

    updates

---
+++ elpa/lsp-go-20180914.515/lsp-go-autoloads.el
@@ -0,0 +1,15 @@
+;;; lsp-go-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("lsp-go.el") (23682 39596 560813 110000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; lsp-go-autoloads.el ends here
---
+++ elpa/protobuf-mode-20170526.950/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20170526.950" "major mode for editing protocol buffers." 'nil :commit "ab95b1bc1eb964ee5f1fb48297344c5d37d35191" :keywords '("google" "protobuf" "languages"))
